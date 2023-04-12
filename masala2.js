8c7d91ec3880116e0eadf964e331a2189787ec97
Sirojiddin <jorisdandii@gmail.com>
2023-04-12 21:41:50 +0900

    first commit

---
+++ masala2.js
@@ -0,0 +1,52 @@
+
+
+// -----2023.04.12------------
+
+// let str = 'siroj';
+// console.log(str.substring(1,1));
+
+// x.trim().split(" ").join("");
+
+// function findSmallestInt(args) {
+//   return args.sort((a, b) => a - b).shift();
+// }
+
+
+// ==================================================================
+// function repeatStr(n, s) {
+//   return new Array(n).fill(s).join("");
+// }
+// ==================================================================
+
+// function repeatStr(n, s) {
+//   return s.repeat(n);
+// }
+// let result = Number('200')
+// console.log(result);
+
+// ==================================================================
+// let year = 1.201;
+// // year.Math.ceil();
+// console.log(Math.tan(year));
+// ==================================================================
+
+// ==================================================================
+// let arr = [true, true, true, false];
+// function countSheeps() {
+//   let str = arr.filter((value) => value === false).length;
+//   console.log(str);
+// }
+// countSheeps()
+// ===================================================================
+
+// ===================================================================
+// let siroj = 100.2;
+// function squareSum(numbers) {
+//   let squareSum = 0;
+//   for (let i = 0; i < siroj.length; i++) {
+//     squareSum += numbers[i] **0;
+//   }
+//   return squareSum;
+// }
+// console.log(squareSum());
+// =====================================================================
