60baa1d0b8406ea90d5821ae0cb049f2e49b31d2
SIROJIDDIN <123286500+Jorisdandii@users.noreply.github.com>
2023-04-23 15:11:26 +0900

    text task

---
+++ tasks_1.js
@@ -0,0 +1,29 @@
+
+
+// kara kara jadvali
+// for (i = 2; i <= 10; i++) {
+//   for (j = 1; j <= 9; j++) {
+//     console.log(`| ${i} x ${j} = ${i * j} |`);
+//   }
+
+//   console.log("//////////////");
+// }
+
+
+// function basicOp(operation, value1, value2) {
+//   if (operation == "+") {
+//     return value1 + value2;
+//   }
+//   if (operation == "-") {
+//     return value1 - value2;
+//   }
+//   if (operation == "*") {
+//     return value1 * value2;
+//   }
+//   if (operation == "/") {
+//     return value1 / value2;
+//   }
+// }
+
+// basicOp(10);
+// console.log(operation);
